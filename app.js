eef8f3335a8ac13902f1dc5d2f2ce91d513e1e7a
Pedro J Colon <64868232+PedroJColon@users.noreply.github.com>
2025-05-19 19:04:58 -0400

    Add cells to be made within grid container and cells change color when hovered

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const numOfCells = 16;
 
 function drawBoard() {
-    for (let y = 0; y <= numOfCells; y++) {
+    const gridContainer = document.querySelector(".grid-container");
+    for (let y = 0; y < numOfCells; y++) {
         console.log("Y Cell Num: " + y);
-        for (let x = 0; x <= numOfCells; x++) {
-            console.log("X Cell Num: " + x);            
+        for (let x = 0; x < numOfCells; x++) {
+            console.log("X Cell Num: " + x);        
+            const cell = document.createElement("div");
+            cell.classList.toggle("cell");
+            gridContainer.appendChild(cell);
         }
     }
 }
 
 drawBoard();
+
+const cells = document.querySelectorAll(".cell");
+cells.forEach(cell => {
+    cell.addEventListener("mouseover", (e) => {
+        e.target.style.backgroundColor = "red";
+    });
+});
