e18acc0da94b8baba9c6b3aea59e7b5fd01a19d0
Pedro J Colon <64868232+PedroJColon@users.noreply.github.com>
2025-05-19 23:12:41 -0400

    Add limit to amount of cells that can be created, along with guardrails to ensure experience is consisent

--- app.js
+++ app.js
@@ -1,13 +1,36 @@
 let numOfCells = 16;
+const maxCellNum = 100;
 
 function drawBoard() {
     const gridContainer = document.querySelector(".grid-container");
-    for (let y = 0; y < numOfCells; y++) {
+
+    if (numOfCells > 16) {
+        const container = document.querySelector(".container");
+        container.style.paddingTop = "12%"
+        gridContainer.style.width = "520px";
+    }
+    else {
+        const container = document.querySelector(".container");
+        container.style.paddingTop = "12%"
+        gridContainer.style.width = "265px";
+    }
+    if (numOfCells > 32) {
+        const container = document.querySelector(".container");
+        container.style.paddingTop = "22%"
+        gridContainer.style.width = "1030px";
+    }
+    if (numOfCells > 64) {
+        const container = document.querySelector(".container");
+        container.style.paddingTop = "30%"
+        gridContainer.style.width = "2060px";
+    }
+    for (let y = 0; y <= numOfCells; y++) {
         console.log("Y Cell Num: " + y);
         for (let x = 0; x < numOfCells; x++) {
             console.log("X Cell Num: " + x);        
             const cell = document.createElement("div");
             cell.classList.toggle("cell");
+            cell.style.height = 
             gridContainer.appendChild(cell);
         }
     }
@@ -37,6 +60,10 @@ const data = document.querySelector("#submit-button");
 data.addEventListener("click", (e) => {
     const userNumber = document.querySelector("input");
     numOfCells = userNumber.value;
+    if (numOfCells > maxCellNum) {
+        alert("Cell number entered exceeds the maximum limit!");
+        return;
+    }
     resetBoard();
     drawBoard();
 });
